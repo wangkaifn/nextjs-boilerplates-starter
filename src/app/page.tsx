ec1bb06fb41599c6f1d1f5d56f61db14a44dbdc6
wangkai <kai150036@163.com>
2025-06-25 21:13:36 +0800

    feat: 集成 Prettier 配置

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="min-h-screen font-[family-name:var(--font-geist-sans)]">
+    <div className='min-h-screen font-[family-name:var(--font-geist-sans)]'>
       <h1>Hello Nextjs</h1>
     </div>
-  );
+  )
 }
