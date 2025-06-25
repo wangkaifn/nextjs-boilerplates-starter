942d04d96a1311e2e1de67a847910a1b17ad3cd9
wangkai <kai150036@163.com>
2025-06-25 20:07:42 +0800

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen font-[family-name:var(--font-geist-sans)]">
+      <h1>Hello Nextjs</h1>
+    </div>
+  );
+}
